237f7fe53067eff15ec3ae3567804bdb033ff7e7
Lee Palacios <vitualizz@gmail.com>
2020-11-12 12:46:05 -0500

    Modularize MediaPlayer

--- assets/index.js
+++ assets/index.js
@@ -1,26 +1,7 @@
+import MediaPlayer from './MediaPlayer.js'
 
-  function MediaPlayer(config) {
-    this.media = config.el;
-  }
+const video = document.querySelector('video');
+const player = new MediaPlayer({ el: video });
 
-  MediaPlayer.prototype.play = function() {
-    this.media.play();
-  };
-
-  MediaPlayer.prototype.pause = function() {
-    this.media.pause();
-  };
-
-  MediaPlayer.prototype.togglePlay = function() {
-    if (this.media.paused) {
-      this.play();
-    } else {
-      this.pause();
-    }
-  };
-
-  const video = document.querySelector('video');
-  const player = new MediaPlayer({ el: video });
-
-  const button = document.querySelector('button');
-  button.onclick = () => player.togglePlay();
+const button = document.querySelector('button');
+button.onclick = () => player.togglePlay();
