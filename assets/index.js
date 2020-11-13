7377d413a9a5da86c89eb59ea273557c68cc61ab
Lee Palacios <vitualizz@gmail.com>
2020-11-13 02:04:04 -0500

    order MediaPlayer new

--- assets/index.js
+++ assets/index.js
@@ -2,12 +2,10 @@ import MediaPlayer from './MediaPlayer.js'
 import AutoPlay from './AutoPlay.js'
 
 const video = document.querySelector('video')
-const player = new MediaPlayer(
-  {
-    el: video,
-    plugins: [new AutoPlay()]
-  }
-)
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay()]
+})
 
 const playBtn = document.getElementById('play')
 const muteBtn = document.getElementById('mute')
